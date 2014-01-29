9962e1d072df1c3c990b9db5799168addc70b891
oleg.legator <oleg.legator@gmail.com>
2014-01-29 23:15:02 +0200

    First day developed
    Only main form UI

---
+++ DayJournal/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+using GalaSoft.MvvmLight;
+
+namespace DayJournal.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+    }
+}
